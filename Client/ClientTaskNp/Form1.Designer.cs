bad21c10c09518350d9c12928c1ef6e4a52cbf30
ahmed khfaga <idkmaster89@gmail.com>
2026-05-20 19:21:58 +0300

    adding Client For server

---
+++ Client/ClientTaskNp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ClientTaskNp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connectbutton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chossebutton = new System.Windows.Forms.Button();
+            this.sendbutton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Connectbutton
+            // 
+            this.Connectbutton.Location = new System.Drawing.Point(669, 97);
+            this.Connectbutton.Name = "Connectbutton";
+            this.Connectbutton.Size = new System.Drawing.Size(96, 30);
+            this.Connectbutton.TabIndex = 0;
+            this.Connectbutton.Text = "Connect";
+            this.Connectbutton.UseVisualStyleBackColor = true;
+            this.Connectbutton.Click += new System.EventHandler(this.Connectbutton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(125, 117);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(493, 263);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(213, 405);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(309, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // chossebutton
+            // 
+            this.chossebutton.Location = new System.Drawing.Point(669, 170);
+            this.chossebutton.Name = "chossebutton";
+            this.chossebutton.Size = new System.Drawing.Size(96, 30);
+            this.chossebutton.TabIndex = 4;
+            this.chossebutton.Text = "Choose File";
+            this.chossebutton.UseVisualStyleBackColor = true;
+            this.chossebutton.Click += new System.EventHandler(this.chossebutton_Click);
+            // 
+            // sendbutton
+            // 
+            this.sendbutton.Location = new System.Drawing.Point(669, 249);
+            this.sendbutton.Name = "sendbutton";
+            this.sendbutton.Size = new System.Drawing.Size(96, 30);
+            this.sendbutton.TabIndex = 5;
+            this.sendbutton.Text = "Send File";
+            this.sendbutton.UseVisualStyleBackColor = true;
+            this.sendbutton.Click += new System.EventHandler(this.sendbutton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sendbutton);
+            this.Controls.Add(this.chossebutton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Connectbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connectbutton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button chossebutton;
+        private System.Windows.Forms.Button sendbutton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Client/ClientTaskNp/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace ClientTaskNp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpClient client;
+        NetworkStream ns;
+        private void Connectbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+
+                client.Connect("127.0.0.1", 5000);
+
+                ns = client.GetStream();
+
+                textBox2.Text = "Connected to server";
+
+                richTextBox1.AppendText("Connected To Server\n");
+            }
+            catch (Exception ex)
+            {
+               textBox2.Text = "Connection Failed: " + ex.Message;
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void chossebutton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+
+                richTextBox1.AppendText("File Selected\n");
+            }
+        }
+
+        private void sendbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = textBox1.Text;
+
+                byte[] fileBytes = File.ReadAllBytes(filePath);
+
+                BinaryWriter bw = new BinaryWriter(ns);
+
+                string fileName = Path.GetFileName(filePath);
+
+                bw.Write(fileName);
+
+                bw.Write(fileBytes.Length);
+
+                bw.Write(fileBytes);
+
+                richTextBox1.AppendText("File Sent To Server\n");
+
+
+
+                BinaryReader br = new BinaryReader(ns);
+
+                int compressedSize = br.ReadInt32();
+
+                byte[] compressedBytes =
+                    br.ReadBytes(compressedSize);
+
+                string savePath = fileName + ".zip";
+
+                File.WriteAllBytes(savePath, compressedBytes);
+
+                richTextBox1.AppendText("Compressed File Received\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
